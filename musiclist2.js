0ccb100d18b0c8ce62722d9ca1036ea7c6c92677
Nidhi <nidhi.m@gmail.com>
2011-09-11 23:38:30 -0400

    added images

--- musiclist2.js
+++ musiclist2.js
@@ -10,52 +10,56 @@ albums:
 	{
 	artist: "Architecture in Helsinki",
 	title:"In Case We Die",
-	image: "img/01.jpg",
+	image: "img/Alt/01.jpg",
 	songs:["Neverevereverdid", "It's 5!", "Tiny Paintings", "Wishbone", "Maybe You Can Owe Me"]
 	},
 	
 	{
 	artist: "Broken Bells",
 	title:"Broken Bells",
-	image: "img/02.jpg",
+	image: "img/Alt/02.jpg",
 	songs:["The High Road", "Vaporize", "Your Head Is on Fire", "The Ghost Inside"]
 	},
 	
 	{
 	artist: "Feist",
 	title:"The Reminder",
-	image: "img/03.jpg",
+	image: "img/Alt/03.jpg",
 	songs:["So Sorry", "I Feel It All", "My Moon My Man", "The Park"]
 	},
 	
 	{
 	artist: "Gorillaz",
 	title:"Plastic Beach",
-	image: "img/04.jpg",
+	image: "img/Alt/04.jpg",
 	songs:["Plastic Beach", "Orchestral Intro", "Welcome to the World", "White Flag"]
 	},
 	
 	{
 	artist: "Grizzly Bear",
 	title:"Veckatimest",
+	image: "img/Alt/05.jpg",
 	songs:["Southern Point", "Two Weeks", "All We Ask", "Fine for Now"]
 	},
 	
 	{
 	artist: "M.I.A.",
 	title:"Arular",
+	image: "img/Alt/06.jpg",
 	songs:["Banana", "Pull Up the People", "Bucky Done Gun", "Sunshowers"]
 	},
 	
 	{
 	artist: "Nouvelle Vague",
 	title:"Bande a Part",
+	image: "img/Alt/07.jpg",
 	songs:["The Killing Moon", "Ever Fallen In Love", "Dance With Me", "Don't Go"]
 	},
 	
 	{
 	artist: "Beck",
 	title:"Guero",
+	image: "img/Alt/08.jpg",
 	songs:["E-Pro", "Que Onda Guero", "Girl", "Missing"]
 	}
 	]
@@ -68,12 +72,14 @@ albums:
 	{
 	artist: "Alois Posch",
 	title:"Schubert Trout Quintet",
+	image: "img/Class/01.jpg",
 	songs:["Piano Quintet in A, Allegro vivace", "Piano Quintet in A, Andante", "Piano Quintet in A 'The Trout', Scherzo", "Piano Quintet in A,  Tema con variazioni", "Piano Quintet in A, Finale"]
 	},
 	
 	{
 	artist: "Maurizio Pollini",
 	title:"Beethoven Piano Sonatas: Moonlight & Pastorale",
+	image: "img/Class/02.jpg",
 	songs:["Andante", "Allegro", "Adagio"]
 	}
 	]
@@ -86,18 +92,21 @@ albums:
 	{
 	artist: "Brazilian Girls",
 	title:"Brazilian Girls",
+	image: "img/Elec/01.jpg",
 	songs:["Homme", "Don't Stop", "Lazy Lover", "Corner Store"]
 	},
 	
 	{
 	artist: "Air",
 	title:"Moon Safari",
+	image: "img/Elec/02.jpg",
 	songs:["La Femme d'Argent", "Sexy Boy", "All I Need", "Kelly Watch the Stars"]
 	},
 	
 	{
 	artist: "Nightmares on Wax",
 	title:"In a Space Outta Sound",
+	image: "img/Elec/03.jpg",
 	songs:["Passion", "The Sweetest", "Flip Ya Lid", "Pudpots"]
 	}
 	]
@@ -222,12 +231,45 @@ document.write('<div id="musiclist">');
 /*console.log(iTunes[0]);
 console.log(iTunes[0].albums[0]);*/
 
-document.write('<div id="alternative">'+'<ul>');
+
 
 function alternative(){
+document.write('<div id="alternative">'+'<ul>');
 var alternative = iTunes[0];
 for(var i=0; i<alternative.albums.length; i++) {
 	var album = alternative.albums[i];
+	/*console.log(genre.albums.length);
+	console.log(album.title);*/
+	document.write('<li>' + album.title + '</li>');
+	document.write('<li class="firstitem">'+'<img src="' + album.image + '">');
+	
+	
+	
+	for(var j=0; j<album.songs.length; j++) {
+		document.write('<ul class="songs">');
+		var song = album.songs[j];
+		console.log(song.title);
+		document.write('<li>' + song + '</li>');
+		document.write('</ul>');
+	}
+	
+	document.write('</li>' );
+	
+	console.log(album.songs.length);
+	
+}
+document.write('<ul>');
+document.write('</div>');
+}
+
+
+
+
+function classical(){
+document.write('<div id="classical">' + '<ul>');
+var classical = iTunes[1];
+for(var i=0; i<classical.albums.length; i++) {
+	var album = classical.albums[i];
 	/*console.log(genre.albums.length);
 	console.log(album.title);*/
 	document.write('<li class="firstitem">'+'<img src="' + album.image + '">'+'</li>' );
@@ -246,49 +288,22 @@ for(var i=0; i<alternative.albums.length; i++) {
 	
 }
 document.write('<ul>');
-
-}
 document.write('</div>');
 
-document.write('<div id="classical">' + '<ul>');
-
-function classical(){
-var classical = iTunes[1];
-for(var i=0; i<classical.albums.length; i++) {
-	var album = classical.albums[i];
-	/*console.log(genre.albums.length);
-	console.log(album.title);*/
-	document.write('<li>' + album.title + '</li><ul>');
-	
-	
-	for(var j=0; j<album.songs.length; j++) {
-		var song = album.songs[j];
-		console.log(song.title);
-		document.write('<li>' + song + '</li>');
-	}
-	document.write('</ul>');
-	
-	
-	console.log(album.songs.length);
-	
 }
-document.write('<ul>');
 
 
-}
-document.write('</div>');
 
-/*
-document.write('<div id="classical">' + iTunes[2].genre + '<ul>');
 
 function electronic(){
-
+document.write('<div id="electronic">' + '<ul>');
 var electronic = iTunes[2];
 for(var i=0; i<electronic.albums.length; i++) {
 	var album = electronic.albums[i];
-	//console.log(genre.albums.length);//
-	//console.log(album.title);//
-	document.write('<li>' + album.title + '</li><ul>');
+	/*console.log(genre.albums.length);
+	console.log(album.title);*/
+	document.write('<li class="firstitem">'+'<img src="' + album.image + '">'+'</li>' );
+	document.write('<li>' + album.title + '</li><ul class="songs">');
 	
 	
 	for(var j=0; j<album.songs.length; j++) {
@@ -303,9 +318,7 @@ for(var i=0; i<electronic.albums.length; i++) {
 	
 }
 document.write('<ul>');
-
 document.write('</div>');
 
-}*/
+}
 
-document.write('</div>');
